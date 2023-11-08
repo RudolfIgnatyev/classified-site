e73cfa5c1f48e9225453860357c7f73dcd2724f0
Rudolf Ignatyev <ignatiev.rudolf2012@yandex.ru>
2023-11-08 21:25:47 +0300

    fix: добавить CSS для кнопки применения фильтрации

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,1 @@
 import './index.css';
-// import { rSlider } from "../global/rSlider.js";
-
-// const mySlider = new rSlider({
-//   target: '#sampleSlider',
-//   values: [10000, 1000000],
-//   range: true,
-//   tooltip: true,
-//   scale: true,
-//   labels: false,
-//   step: 10000
-// });
